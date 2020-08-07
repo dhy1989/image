57c60c7d0baf0c040751a17d4b8f2d10a57919d5
dinghy <88888888>
2020-08-07 09:16:39 +0800

    修改

--- src/main/java/com/example/demo/controller/HelloController.java
+++ src/main/java/com/example/demo/controller/HelloController.java
@@ -1,5 +1,6 @@
 package com.example.demo.controller;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -9,8 +10,11 @@ import org.springframework.web.bind.annotation.RestController;
  */
 @RestController
 public class HelloController {
+    @Value("${hello}")
+    private String hello;
+
     @GetMapping("hello")
     public String hello(){
-        return "hello world!!!";
+        return hello;
     }
 }
